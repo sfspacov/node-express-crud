637252e0a89127af8ccbef6115ac9c18a55138a3
stephan-caylent <117462968+stephan-caylent@users.noreply.github.com>
2024-03-26 23:18:12 -0500

    form improved

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ const url = "mongodb://localhost:27017"; // Replace with your MongoDB connection
 mongoose.connect(url, { useNewUrlParser: true });
 const con = mongoose.connection;
 
-var whitelist = ['file:///C:/src/node-express-crud/index.html'];
-
+const whitelist = ['http://127.0.0.1:5500']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 // Configure CORS options to allow any origin
-const corsOptions = {
-    origin: '*'
-  };
+// const corsOptions = {
+//     origin: '*'
+//   };
 
 app.use(cors(corsOptions)); // Apply CORS middleware with options
 
